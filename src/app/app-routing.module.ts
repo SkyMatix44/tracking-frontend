43f454b8d740d14adbab8adf59d3824225d2f052
daw55 <david.weber@student.uni-siegen.de>
2022-04-23 21:01:37 +0200

    add routing to all components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ProjectAnalyticsComponent } from './project-analytics/project-analytics.component';
+import { ProjectListreportComponent } from './project-listreport/project-listreport.component';
+import { ProjectOverviewComponent } from './project-overview/project-overview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'overview',
+    component: ProjectOverviewComponent,
+  },
+  {
+    path: 'analytics',
+    component: ProjectAnalyticsComponent,
+  },
+  {
+    path: 'listreport',
+    component: ProjectListreportComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NewsTileComponent } from './dashboard/news-tile/news-tile.component';
+import { NotesTileComponent } from './dashboard/notes-tile/notes-tile.component';
+import { StudyTileComponent } from './dashboard/study-tile/study-tile.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { ProjectOverviewComponent } from './project-overview/project-overview.component';
 import { ProjectAnalyticsComponent } from './project-analytics/project-analytics.component';
 import { ProjectListreportComponent } from './project-listreport/project-listreport.component';
-import { NewsTileComponent } from './dashboard/news-tile/news-tile.component';
-import { StudyTileComponent } from './dashboard/study-tile/study-tile.component';
-import { NotesTileComponent } from './dashboard/notes-tile/notes-tile.component';
+import { ProjectOverviewComponent } from './project-overview/project-overview.component';
 import { RegisterComponent } from './register/register.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +34,15 @@ import { RegisterComponent } from './register/register.component';
     NotesTileComponent,
     RegisterComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
