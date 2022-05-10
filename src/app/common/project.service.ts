a82bc7147142d18306d6de4301d145aed47e0a20
SkyMatix44 <timschmitt97.ts@gmail.com>
2022-05-10 16:39:01 +0200

    hold projects and current project id in service

--- src/app/common/project.service.ts
+++ src/app/common/project.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, combineLatest, map, Observable } from 'rxjs';
 import { HttpService } from './http.service';
 import { User } from './user.service';
 
@@ -7,6 +7,16 @@ import { User } from './user.service';
   providedIn: 'root',
 })
 export class ProjectService {
+  // current selected project id, -1 when no project selected
+  public readonly currentProjectId$: BehaviorSubject<number> =
+    new BehaviorSubject(-1);
+
+  // TODO load and set projects after login
+  // projects of the current user
+  public readonly projects$: BehaviorSubject<Project[]> = new BehaviorSubject(
+    [] as Project[]
+  );
+
   constructor(private httpService: HttpService) {}
 
   /**
@@ -52,6 +62,64 @@ export class ProjectService {
     option: 'all' | 'participants' | 'scientists' = 'all'
   ): Observable<User[]> {
     return this.httpService.get<User[]>(`project/${projectId}/users/${option}`);
+  }
+
+  /**
+   * Set current project id
+   */
+  setCurrentProjectId(projectId: number): void {
+    this.currentProjectId$.next(projectId);
+  }
+
+  /**
+   * Returns the current project id, -1 when no project selected
+   */
+  getCurrentProjectId(): number {
+    return this.currentProjectId$.getValue();
+  }
+
+  /**
+   * Returns the current project
+   */
+  getCurrentProject(): Project | null {
+    const projectId = this.getCurrentProjectId();
+    if (projectId > -1) {
+      const project = this.projects$.getValue().find((p) => p.id === projectId);
+      if (project) {
+        return project;
+      }
+    }
+
+    return null;
+  }
+
+  /**
+   * Set projects
+   */
+  setProjects(projects: Project[]): void {
+    this.projects$.next(projects);
+  }
+
+  /**
+   * Returns an observable with the current project
+   */
+  getCurrentProjectObs(): Observable<Project | null> {
+    return combineLatest([this.currentProjectId$, this.projects$]).pipe(
+      map(([projectId, projects]) => {
+        if (projectId > -1 && projects.length > 0) {
+          const curProject = this.projects$
+            .getValue()
+            .find((p) => p.id === projectId);
+
+          if (curProject) {
+            return curProject;
+          }
+        }
+
+        return null;
+      })
+      // shareReplay()
+    );
   }
 }
 
